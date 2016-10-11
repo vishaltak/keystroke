afa89cd06f0d8d29b0a9a107f47c4f716f4f6f90
Vishal Tak <vishaltak95@gmail.com>
2016-10-11 21:41:46 +0530

    First Commit. Install the module 'pyxhook' and 'Xlib'. 'pyxhook.py' contains the main implementation details of the keylogger. 'gettimelog.py' is the main file which wries the timestamp data into the files in the 'data' folder

---
+++ gettimelog.py
@@ -0,0 +1,45 @@
+import time
+import pyxhook
+
+def kbpressevent( event ):
+    #print key info
+    #print(event)
+    
+    global raw_press
+    raw_press.write(str(event)+"\n")
+    
+    #If the ascii value matches enter, terminate the while loop
+    if event.Ascii == 0:
+        global running
+        running = False
+
+def kbreleaseevent( event ):
+	#print key info
+    #print(event)
+
+    global raw_release
+    raw_release.write(str(event)+"\n")
+
+#Create hookmanager
+hookman = pyxhook.HookManager()
+
+
+#Define our callback to fire when a key is pressed down
+hookman.KeyDown = kbpressevent
+#Define our callback to fire when a key is released
+hookman.KeyUp = kbreleaseevent
+#Hook the keyboard
+hookman.HookKeyboard()
+#Start our listener
+hookman.start()
+    
+#Open the files to enter the data
+raw_press = open("/home/tak/Desktop/keylog/data/raw_press.txt" , "w")
+raw_release = open("/home/tak/Desktop/keylog/data/raw_release.txt", "w")
+#Create a loop to keep the application running
+running = True
+while running:
+	time.sleep(0.1)
+
+#Close the listener when we are done
+hookman.cancel()
