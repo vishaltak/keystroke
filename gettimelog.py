0048abdf58153c621759c07fd00bd9eb1b0069a4
Vishal Tak <vishaltak95@gmail.com>
2016-10-12 11:31:16 +0530

    Equal no. of readings in both press and release files

--- gettimelog.py
+++ gettimelog.py
@@ -2,25 +2,18 @@ import time
 import pyxhook
 
 def kbpressevent( event ):
-    #print key info
-    #print(event)
     
+    #print key info
     global raw_press
     #When the key is Backspace, it is represented by a prefix "-" in the file. So that line and the preevious line is not to be considered
     if event.Ascii == 8:
         raw_press.write("-"+ str(event)+"\n")
     else:
         raw_press.write(str(event)+"\n")
-    
-    #If the ascii value matches enter, terminate the while loop
-    if event.Ascii == 13:
-        global running
-        running = False
 
 def kbreleaseevent( event ):
-	#print key info
-    #print(event)
-
+	
+    #print key info
     global raw_release
     #When the key is Backspace, it is represented by a prefix "-" in the file. So that line and the preevious line is not to be considered
     if event.Ascii == 8:
@@ -28,9 +21,13 @@ def kbreleaseevent( event ):
     else:
         raw_release.write(str(event)+"\n")
 
+    #If the ascii value matches enter, terminate the while loop
+    if event.Ascii == 13:
+        global running
+        running = False
+
 #Create hookmanager
 hookman = pyxhook.HookManager()
-
 
 #Define our callback to fire when a key is pressed down
 hookman.KeyDown = kbpressevent
@@ -42,8 +39,11 @@ hookman.HookKeyboard()
 hookman.start()
     
 #Open the files to enter the data
-raw_press = open("/home/tak/Desktop/keylog/data/raw_press.txt" , "w")
-raw_release = open("/home/tak/Desktop/keylog/data/raw_release.txt", "w")
+#raw_press = open("/home/tak/Desktop/keystroke/data/raw_press.txt" , "w")
+#raw_release = open("/home/tak/Desktop/keystroke/data/raw_release.txt", "w")
+raw_press = open("<file_location>" , "w")
+raw_release = open("<file_location>", "w")
+
 #Create a loop to keep the application running
 running = True
 while running:
