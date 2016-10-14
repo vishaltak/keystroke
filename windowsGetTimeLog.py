a6070198fc977771ef75b8e0a676c1de51dc3318
Vishal Tak <vishaltak95@gmail.com>
2016-10-15 00:10:32 +0530

    Hotfix for Windows keylog support

--- windowsGetTimeLog.py
+++ windowsGetTimeLog.py
@@ -1,29 +1,39 @@
+from datetime import datetime, timedelta
+import calendar
 import time
 import pyHook
 import pythoncom
+import win32api
+import wmi
+
+wmiob = wmi.WMI()
+sdata = wmiob.Win32_PerfFormattedData_PerfOS_System()
+uptime = sdata[-1].SystemUpTime
+tnow = datetime.now()
+utime = timedelta(seconds=int(uptime))
+boot = tnow-utime
+boot = calendar.timegm(boot.utctimetuple())
 
 def kbpressevent( event ):
-    
+    global boot
     #print key info
     global raw_press
     #When the key is Backspace, it is represented by a prefix "-" in the file. So that line and the preevious line is not to be considered
     if event.Ascii == 8:
-        raw_press.write("-"+ str(event)+"\n")
+        raw_press.write("-"+ str(event.Time + boot)+"\n")
     else:
-        raw_press.write(str(event)+"\n")
-    print("ese")
-    print(event)
+        raw_press.write(str(event.Time + boot)+"\n")
     return 0
 
 def kbreleaseevent( event ):
-	
+    global boot
     #print key info
     global raw_release
     #When the key is Backspace, it is represented by a prefix "-" in the file. So that line and the preevious line is not to be considered
     if event.Ascii == 8:
-        raw_release.write("-"+ str(event)+"\n")
+        raw_release.write("-"+ str(event.Time + boot)+"\n")
     else:
-        raw_release.write(str(event)+"\n")
+        raw_release.write(str(event.Time + boot)+"\n")
 
     #If the ascii value matches enter, terminate the while loop
     if event.Ascii == 13:
