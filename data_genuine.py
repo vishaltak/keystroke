7979a36f124d50ff86c0c5baa06a8eb0d6235f37
Vishal Tak <vishaltak95@gmail.com>
2017-01-29 23:14:30 +0530

    removed extra spaces from CSVs. also removed extra spaces from the scripts creating the CSV

--- data_genuine.py
+++ data_genuine.py
@@ -12,7 +12,7 @@ with open(save_path + "genuine.txt", "w") as av:
 		fd = f.read().splitlines()
 	feildnames = fd[0]
 	names = feildnames.split()
-	fields = ', '.join(names)
+	fields = ','.join(names)
 	av.write(fields + '\n')
 	for user in os.listdir(database_folder):
 		s_type = ["/genuine/"]
@@ -24,54 +24,54 @@ with open(save_path + "genuine.txt", "w") as av:
 				for session in os.listdir(database_folder + user + session_type):
 					if os.path.isdir(database_folder + user + session_type + session):
 
-						av.write(str(int(user[-3:len(user)])) + ", ")
+						av.write(str(int(user[-3:len(user)])) + ",")
 
 						with open(database_folder + user + session_type + session + r'/date.txt') as f:
 							date = f.readlines() 
-							av.write(date[0]+ ", ")
+							av.write(date[0]+ ",")
 						
 						with open(database_folder + user + session_type + session + r'/genuine.txt') as f:
 							genuine = f.readlines() 
-							av.write(genuine[0] + ", ")
+							av.write(genuine[0] + ",")
 						
 						with open(database_folder + user + session_type + session + r'/password.txt') as f:
 							password = f.readlines() 
-							av.write(password[0] + ", ")
+							av.write(password[0] + ",")
 						
 						with open(database_folder + user + session_type + session + r'/p_release_codes.txt') as f:
 							release = f.read().splitlines()
 						r = release[0]
 						rcode = r.split()
 						rc = ' '.join(rcode)
-						av.write(rc + ', ')
+						av.write(rc + ",")
 						
 						with open(database_folder + user + session_type + session + r'/pp.txt') as f:
 							temp = []
 							for line in f:
 								temp.append(line.strip('\n'))
 							pp = ' '.join(temp)
-						av.write(pp + ', ')
+						av.write(pp + ",")
 						
 						with open(database_folder + user + session_type + session + r'/pr.txt') as f:
 							temp = []
 							for line in f:
 								temp.append(line.strip('\n'))
 							pr = ' '.join(temp)
-						av.write(pr+ ', ')
+						av.write(pr+ ",")
 						
 						with open(database_folder + user + session_type + session + r'/rp.txt') as f:
 							temp = []
 							for line in f:
 								temp.append(line.strip('\n'))
 							rp = ' '.join(temp)
-						av.write(rp+ ', ')
+						av.write(rp+ ",")
 						
 						with open(database_folder + user + session_type + session + r'/rr.txt') as f:
 							temp = []
 							for line in f:
 								temp.append(line.strip('\n'))
 							rr = ' '.join(temp)
-						av.write(rr+ ', ')
+						av.write(rr+ ",")
 
 						with open(database_folder + user + session_type + session + r'/pp.txt') as f:
 							data = []
@@ -80,7 +80,7 @@ with open(save_path + "genuine.txt", "w") as av:
 								rowdata = map(float, fields)
 								data.extend(rowdata)
 							avgpp = sum(data)/len(data)
-						av.write("%d, " %avgpp)
+						av.write("%d," %avgpp)
 
 						with open(database_folder + user + session_type + session + r'/pr.txt') as f:
 							data = []
@@ -91,7 +91,7 @@ with open(save_path + "genuine.txt", "w") as av:
 							avgpr = sum(data)/len(data)
 							#print(data)
 							#print(avgpr)
-						av.write("%d, " %avgpr)
+						av.write("%d," %avgpr)
 
 						with open(database_folder + user + session_type + session + r'/rp.txt') as f:
 							data = []
@@ -100,7 +100,7 @@ with open(save_path + "genuine.txt", "w") as av:
 								rowdata = map(float, fields)
 								data.extend(rowdata)
 							avgrp = sum(data)/len(data)
-						av.write("%d, " %avgrp)
+						av.write("%d," %avgrp)
 
 						with open(database_folder + user + session_type + session + r'/rr.txt') as f:
 							data = []
@@ -109,7 +109,7 @@ with open(save_path + "genuine.txt", "w") as av:
 								rowdata = map(float, fields)
 								data.extend(rowdata)
 							avgrr = sum(data)/len(data)
-						av.write("%d, " %avgrr)
+						av.write("%d," %avgrr)
 						
 						with open(database_folder + user + session_type + session + r'/total.txt') as f:
 							total = f.readlines()
